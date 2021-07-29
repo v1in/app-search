f8faa4a82a7f0650cca6f3499439f2b6626e1c21
VA <inpersona.v@gmail.com>
2021-07-29 17:05:42 +0200

    add custom Loader and .env file

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -2,9 +2,10 @@ import Search from '../Search';
 import styles from './Container.module.css';
 import useFetch from '../../hooks/useFetch';
 import clsx from 'clsx';
+import Loader from '../Loader';
 
 const Container = () => {
-  const {data, error} = useFetch('https://jsonplaceholder.typicode.com/users');
+  const {data, error, loading} = useFetch(`${process.env.REACT_APP_USERS_URL}`);
 
   if (!data) {
     return null;
@@ -14,11 +15,15 @@ const Container = () => {
     <div className={styles.container}>
       <h1 className={styles.textCenter}>Users list</h1>
 
-      <Search
-        searchData={data}
-        placeholder='Search by user name...'
-        classes={clsx({[styles.error]: error})}
-      />
+      {!loading && (
+        <Search
+          searchData={data}
+          placeholder='Search by user name...'
+          classes={clsx({[styles.error]: error})}
+        />
+      )}
+
+      {loading && <Loader />}
 
       {error && <pre>{error}</pre>}
     </div>
---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,5 @@
+import styles from './Loader.module.css';
+
+const Loader = () => <div className={styles.root}>Loading...</div>;
+
+export default Loader;
